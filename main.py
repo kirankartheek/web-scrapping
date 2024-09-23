54f30c4ccee5636e6b9b55e55bb6cdc0f92723af
Apple <apple@Apples-MacBook-Pro.local>
2024-09-23 23:23:03 +0530

    remove unused imports

--- main.py
+++ main.py
@@ -5,21 +5,4 @@ from rest.scrape_products import router as scrape_products
 
 app = FastAPI()
 
-
-# @app.get("/scrape")
-# async def scrape(url: str):
-#     # Fetch the web page
-#     response = requests.get(url)
-#     if response.status_code == 200:
-#         # Parse the HTML content using BeautifulSoup
-#         soup = BeautifulSoup(response.content, "html.parser")
-#
-#         # Example: Extract the page title
-#         page_title = soup.title.string if soup.title else "No title found"
-#
-#         return {"page_title": page_title}
-#     else:
-#         return {"error": "Failed to retrieve the page"}
-
-
 app.include_router(scrape_products, prefix="/api")
